cf68fa492c45b35e73852ae91963a499709a09ec
Alex Pletzer <alexander@gokliya.net>
2023-05-04 07:44:31 +0000

    adding the job_id info to the table

--- bin/analyse.R
+++ bin/analyse.R
@@ -3,15 +3,15 @@ library(ggplot2)
 data <- read.csv('table.txt', sep='|')
 
 # manually assigning column names
-colnames(data) <- c('cycle', 'method', 'submit_num', 'start_time', 'end_time')
+colnames(data) <- c('job_id', 'cycle', 'method', 'submit_num', 'start_time', 'end_time')
 
 # remove rows we are not interested in
 d2 <- data[!(rownames(data) %in% c('install', 'analyse')),]
 #d2 <- data[data$method %in% c('serial', 'shared', 'distributed'),]
 
 # turn the strings into date time objects
-start_time <- as.POSIXct(d2[, 4], format="%Y-%m-%dT%H:%M:%S")
-end_time <- as.POSIXct(d2[, 5], format="%Y-%m-%dT%H:%M:%S")
+start_time <- as.POSIXct(d2$start_time, format="%Y-%m-%dT%H:%M:%S")
+end_time <- as.POSIXct(d2$end_time, format="%Y-%m-%dT%H:%M:%S")
 
 # compute the execution time
 d2$exec_time <- as.numeric(end_time - start_time) # in sec
