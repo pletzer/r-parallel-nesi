f78ed28791e6680181590aa27df6b916f96c0700
Alex Pletzer <alexander@gokliya.net>
2023-05-05 21:23:52 +1200

    + aligned to the oother tests

--- distributed_memory/distributed2.R
+++ distributed_memory/distributed2.R
@@ -8,9 +8,10 @@ if (length(args) >= 1) {
     ntasks <- strtoi(args[1])
 }
 
- 
-slaves <- as.numeric(Sys.getenv(c("SLURM_NTASKS")))-1
-{ sink("/dev/null"); cl <- makeCluster(slaves, type="MPI"); sink(); } # number of MPI tasks to use
+slaves <- as.numeric(Sys.getenv(c("SLURM_NTASKS"))) - 1
+
+cl <- makeCluster(slaves, type="MPI")
+
 registerDoParallel(cl)
  
 tick <- proc.time()
@@ -19,5 +20,9 @@ x <- foreach(z = 1000000:(1000000 + ntasks), .combine=c) %dopar% {
 }
 tock <- proc.time() - tick
  
+#mpi.close.Rslaves()
+#snow::stopCluster(cl)
+Rmpi::mpi.finalize()
+
 cat("\nforeach w/ Rmpi test times using", slaves, "MPI slaves: \n")
 tock
